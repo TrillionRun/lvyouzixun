8e9571bcb1dfcd15080b0f84a4087f017365edde
qingmulanyun <wenbo.zhao@autodesk.com>
2016-10-09 22:16:05 +0800

    fix user policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -9,12 +9,12 @@ class UserPolicy < ApplicationPolicy
   end
 
   def update?
-    return true if user.admin?
+    # return true if user.admin?
     return true if record.id == user.id
   end
 
   def destroy?
-    return true if user.admin?
+    # return true if user.admin?
     return true if record.id == user.id
   end
 
