13495156c3ac533984aa73ed8f9aae3d385c077f
Matt Hu <shuai.matt.hu@outlook.com>
2016-11-14 23:10:36 +0800

    remove redis yml

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 puts '####', ENV["REDISTOGO_URL"], '$$$$$$'
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:url => uri)
