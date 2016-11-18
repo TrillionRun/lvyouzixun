cf47c122344f9dea2df04a131c7b30a69f5cb1eb
mac os <macos@macmatoMacBook-Pro.local>
2016-11-18 21:14:36 +0800

    petition api

--- app/controllers/api/petitions_controller.rb
+++ app/controllers/api/petitions_controller.rb
@@ -2,16 +2,19 @@ class Api::PetitionsController < Api::AdminApiController
   # before_action :authenticate_user!
 
   def create
-    options = petitions_params
-    petition = Petition.create! options
+    petition = Petition.new petitions_params
+    petition.status = 'verifying'
+    petition.save!
     render json: petition, status: 200
   end
 
   def index
     user = User.find(params[:user_id])
-    petitions = Petition.where('user_id = ?', user.id)
+    petitions = Petition.where(user_id: user.id)
     render json: petitions, status: 200
   end
+
+  private
 
   def petitions_params
     columns = [:name, :contact, :contact_phone, :email, :user_id,:company_type_id, :status]
