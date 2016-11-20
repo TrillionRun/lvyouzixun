3263c870bb5000b9553081c5bdea3a893d8e0348
mac os <macos@macmatoMacBook-Pro.local>
2016-11-20 16:36:30 +0800

    users ui

--- app/controllers/customer_service/user_controller.rb
+++ app/controllers/customer_service/user_controller.rb
@@ -10,6 +10,7 @@ class CustomerService::UserController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
@@ -22,6 +23,12 @@ class CustomerService::UserController < ApplicationController
   end
 
   def update
+    @user = User.find params[:id]
+    if @user.update user_params
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def destroy
