587f2ed42a48cc56e236ef034976b8637c1ebdf0
Matt Hu <shuai.matt.hu@outlook.com>
2016-12-04 21:37:39 +0800

    update daily plan

--- app/controllers/customer_service/itinerary_controller.rb
+++ app/controllers/customer_service/itinerary_controller.rb
@@ -10,9 +10,13 @@ class CustomerService::ItineraryController < ApplicationController
     @business = Business.find_by_id params[:business_id]
     @itinerary = @business.itineraries.find_by_id params[:id]
     @daily_plans = @itinerary.daily_plans
-    @daily_plans.sort do |n1, n2|
-      Date.parse(n1.dates) <=> Date.parse(n2.dates) if n1.dates.present? && n1.dates.present?
-    end
+    # @daily_plans.sort do |n1, n2|
+    #   begin
+    #     Date.parse(n1.dates) <=> Date.parse(n2.dates) if n1.dates.present? && n1.dates.present?
+    #   rescue
+    #     1
+    #   end
+    # end
   end
 
   def create
